81788ecda8f1ce49e05c225cf1bd0846cd436589
anynew <anynew.lei@gmail.com>
2016-12-21 17:11:48 +0800

    12.21

--- app/src/main/java/com/ui/anynew/uicomponents/ui/HeartView.java
+++ app/src/main/java/com/ui/anynew/uicomponents/ui/HeartView.java
@@ -1,12 +1,15 @@
 package com.ui.anynew.uicomponents.ui;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PathMeasure;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 
 /**
  * Created by anynew on 2016/12/19.
@@ -20,13 +23,17 @@ public class HeartView extends View {
     // 爱心下端点
     private static final int[] BOTTOM_POINT = new int[]{300, 400};
     // 左侧控制点
-    private static final int[] LEFT_CONTROL_POINT = new int[]{350, 200};
+    private static final int[] LEFT_CONTROL_POINT = new int[]{450, 200};
     // 右侧控制点
     private static final int[] RIGHT_CONTROL_POINT = new int[]{150, 200};
+
+    private float[] mCurrentPosition = new float[2];
 
     private Paint mPaint;
 
     private Path mPath;
+    private PathMeasure mPathMeasure;
+    private Paint nPaint;
 
     public HeartView(Context context) {
         this(context, null);
@@ -42,13 +49,21 @@ public class HeartView extends View {
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeWidth(PATH_WIDTH);
+        mPaint.setStrokeWidth(5);
         mPaint.setColor(Color.RED);
+
+        nPaint = new Paint();
+        nPaint.setStyle(Paint.Style.STROKE);
+        nPaint.setStrokeWidth(8);
+        nPaint.setColor(Color.BLUE);
 
         mPath = new Path();
         mPath.moveTo(START_POINT[0], START_POINT[1]);
         mPath.quadTo(RIGHT_CONTROL_POINT[0], RIGHT_CONTROL_POINT[1], BOTTOM_POINT[0],
                 BOTTOM_POINT[1]);
         mPath.quadTo(LEFT_CONTROL_POINT[0], LEFT_CONTROL_POINT[1], START_POINT[0], START_POINT[1]);
+
+        mPathMeasure = new PathMeasure(mPath,false);
     }
 
     @Override
@@ -58,5 +73,24 @@ public class HeartView extends View {
         canvas.drawPath(mPath, mPaint);
         canvas.drawCircle(RIGHT_CONTROL_POINT[0], RIGHT_CONTROL_POINT[1], 5, mPaint);
         canvas.drawCircle(LEFT_CONTROL_POINT[0], LEFT_CONTROL_POINT[1], 5, mPaint);
+
+        canvas.drawCircle(mCurrentPosition[0],mCurrentPosition[1],8,nPaint);
+    }
+    public void startPathAnim(){
+
+        ValueAnimator animator = ValueAnimator.ofFloat(0,mPathMeasure.getLength());
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(3000);
+        animator.setRepeatCount(Integer.MAX_VALUE);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (float)animation.getAnimatedValue();
+                mPathMeasure.getPosTan(value,mCurrentPosition,null);
+
+                postInvalidate();
+            }
+        });
+        animator.start();
     }
 }
--- app/src/main/java/com/ui/anynew/uicomponents/ui/TextView.java
+++ app/src/main/java/com/ui/anynew/uicomponents/ui/TextView.java
@@ -30,19 +30,12 @@ public class TextView extends View {
 
     private float baseX = 200;
 
-    private String string = "nihao,glass";
-
     private float[] cir_location;
 
-    private float progress;
-
-    private float startX = 0, startY = 0;
     private Path mPath;
     private Paint mPaint;
     private PathMeasure mPathMeasure;
     private float length;
-    private DashPathEffect mEffect;
-    private Path nPath;
 
     public TextView(Context context) {
         this(context, null);
@@ -52,7 +45,7 @@ public class TextView extends View {
         super(context, attrs);
         basePaint = new Paint();
         basePaint.setStyle(Paint.Style.STROKE);
-        basePaint.setStrokeWidth(5);
+        basePaint.setStrokeWidth(20);
         basePaint.setAntiAlias(true);
         basePaint.setColor(Color.BLUE);
 
@@ -63,14 +56,14 @@ public class TextView extends View {
 
         cir_location = new float[2];
 
-
-       /* mPath.moveTo(250, 250);
+        mPath = new Path();
+        mPath.moveTo(250, 250);
         mPath.lineTo(450, 250);
         mPath.lineTo(550, 380);
         mPath.lineTo(450, 510);
         mPath.lineTo(250, 510);
         mPath.lineTo(150, 380);
-        mPath.close();*/
+        mPath.close();
 
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
@@ -78,6 +71,9 @@ public class TextView extends View {
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeWidth(5);
 
+//        mPath.addCircle(350,350,200, Path.Direction.CW);
+        mPathMeasure = new PathMeasure(mPath, false);
+        length = mPathMeasure.getLength();
 
     }
 
@@ -85,37 +81,27 @@ public class TextView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        mPath = new Path();
-        RectF rectF = new RectF(getPaddingLeft(), getPaddingTop(), getWidth() - getPaddingRight(), getHeight() - getPaddingBottom());
-        mPath.addOval(rectF, Path.Direction.CCW);
-        mPathMeasure = new PathMeasure(mPath, false);
-        length = mPathMeasure.getLength();
         canvas.drawPath(mPath, mPaint);
-     /*   Path path = new Path();
-        path.moveTo(300, 300);
-        path.lineTo(300 + progress,300+ progress);
-
-        canvas.drawPath(path, basePaint);*/
-        PathMeasure pathMeasure = new PathMeasure(mPath, false);
-        pathMeasure.getPosTan(pathMeasure.getLength(), cir_location, null);
-        canvas.drawCircle(cir_location[0], cir_location[1], 8, tPaint);
+        canvas.drawCircle(cir_location[0], cir_location[1], 10, tPaint);
 
         Log.e("TAG", "cir_location[0]: " + cir_location[0] + " cir_location[1] :" + cir_location[1]);
 
     }
 
     public void startAnim() {
-        ValueAnimator animator = ValueAnimator.ofFloat(1, 0);
+        ValueAnimator animator = ValueAnimator.ofFloat(0,length);
         animator.setDuration(3000);
-        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.setRepeatCount(Integer.MAX_VALUE);
+        animator.setInterpolator(new LinearInterpolator());
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                progress = (float) animation.getAnimatedValue();
-                mEffect = new DashPathEffect(new float[]{length, length}, progress * length);
-                mPaint.setPathEffect(mEffect);
+                float value = (float) animation.getAnimatedValue();
+                mPathMeasure.getPosTan(value, cir_location, null);
+//                mEffect = new DashPathEffect(new float[]{length, length}, progress * length);
+//                mPaint.setPathEffect(mEffect);
                 postInvalidate();
-                Log.e("TAG", "progress value is " + progress);
+                Log.e("TAG", "progress value is " + value);
             }
         });
         animator.start();
